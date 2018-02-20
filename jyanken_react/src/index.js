1027f61cb85a77c37ec1b1f52d7c9a49dacafb50
fuser3-HP\narutani <n85dnhf5cl3w@sute.jp>
2018-02-20 09:35:07 +0900

    ok

--- jyanken_react/src/index.js
+++ jyanken_react/src/index.js
@@ -1,51 +1,108 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import './index.css'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton'
+import Paper from 'material-ui/Paper';
+import {Tabs,Tab}from 'material-ui/Tabs'
+import {Table,TableBody,TableHeader,TableHeaderColumn,TableRow,TableRowColumn} from 'material-ui/Table';
+import Jyanken from './Jyanken'
+// import './index.css'
 
 class JyankenGamePage extends Component{
     constructor(props){
         super(props)
-        this.state = {human: null, computer: null}
+        // this.state = {human: null, computer: null}
+        this.jyanken = new Jyanken()
+        this.state = {scores: [], status: {}, tabIndex: 0}
     }
-    pon(human_hand){
-        const computer_hand = Math.floor(Math.random()*3)
-        this.setState({human: human_hand,computer: computer_hand})
-    }
-    judge(){
-        if(this.state.human==null){
-            return null
-        }else{
-            return (this.state.computer - this.state.human + 3) % 3
-        }
+    // pon(human_hand){
+    //     const computer_hand = Math.floor(Math.random()*3)
+    //     this.setState({human: human_hand,computer: computer_hand})
+    // }
+    // judge(){
+    //     if(this.state.human==null){
+    //         return null
+    //     }else{
+    //         return (this.state.computer - this.state.human + 3) % 3
+    //     }
+    // }
+
+    // render(){
+    //     return(
+    //         <div>
+    //             <h1>じゃんけん　ポン！</h1>
+    //             <JyankenBox actionPon = {(te) => this.pon(te) } />
+    //             <ScoreBox human = {this.state.human} computer={this.state.computer} judgment={this.judge()} />                
+    //         </div>
+    //     )
+    // }
+    componentDidMount(){
+        // setTimeout(() => {this.pon(1)},1000 )
+
+        this.getResult()
     }
 
+//     shouldComponentUpdate(nextProps, nextState){
+//         const identical = nextState.human == this.state.human &&
+//         nextState.computer == this.state.computer
+//         if(identical){console.log("*Identical*")}
+//         return !identical
+//     }
+
+    tabChange(ix){
+        this.setState({scores: this.jyanken.getScores()})
+        this.getResult()
+    }
     render(){
-        return(
-            <div>
-                <h1>じゃんけん　ポン！</h1>
-                <JyankenBox actionPon = {(te) => this.pon(te) } />
-                <ScoreBox human = {this.state.human} computer={this.state.computer} judgment={this.judge()} />                
-            </div>
+        return (
+            <MuiThemeProvider>
+                <div style={{marginLeft: 30}}>
+                <header>じゃんけん　ポン！</header>
+                <JyankenBox　actionPon={(te) => this.pon(te)} />
+                    <Paper style={{width: 400}} zDepth={2}>
+                        <Tabs value={this.state.tabIndex} onChange={(ix) => this.tabChange(ix)} >
+                            <Tab label="" value={0}>
+                                <ScoreList scores={this.state.scores} />
+                            </Tab>
+                        </Tabs>
+                    </Paper>
+                </div>
+            </MuiThemeProvider>
         )
-    }
-    componentDidMount(){
-        setTimeout(() => {this.pon(1)},1000 )
-    }
-    shouldComponentUpdate(nextProps, nextState){
-        const identical = nextState.human == this.state.human &&
-        nextState.computer == this.state.computer
-        if(identical){console.log("*Identical*")}
-        return !identical
     }
 }
 
+const StatusBox = (props) => (
+    <Table>
+        <TableBody displayRowCheckbox={false}>
+            <TableRow displayBorder={false}>
+                <TableHeaderColumn>勝ち</TableHeaderColumn><TableRowColumn style={judgmentStyle(1)} > {props.status.win}</TableRowColumn>
+            </TableRow>
+            <TableRow displayBorder={false}>
+                <TableHeaderColumn>負け</TableHeaderColumn><TableRowColumn style={judgmentStyle(2)} > {props.status.lose}</TableRowColumn>
+            </TableRow>
+            <TableRow displayBorder={false}>
+                <TableHeaderColumn>引き分け</TableHeaderColumn><TableRowColumn style={judgmentStyle(0)} > {props.status.draw}</TableRowColumn>
+            </TableRow>
+        </TableBody>
+    </Table>
+)
+
+
+
 const JyankenBox = (props) => {
+    const style = {marginLeft:20}
     return(
-        <div>
-            <button onClick={() => props.actionPon(0)} >グー</button>
-            <button onClick={() => props.actionPon(1)} >チョキ</button>
-            <button onClick={() => props.actionPon(2)} >パー</button>
+        // <div>
+        //     <button onClick={() => props.actionPon(0)} >グー</button>
+        //     <button onClick={() => props.actionPon(1)} >チョキ</button>
+        //     <button onClick={() => props.actionPon(2)} >パー</button>
+        // </div>
+        <div style={{marginTop:40, marginBottom:30, marginLeft:30}}>
+            <RaisedButton label="グー" onClick={() => props.actionPon(0)} style={style} />
+            <RaisedButton label="チョキ" onClick={() => props.actionPon(1)} style={style} />
+            <RaisedButton label="パー" onClick={() => props.actionPon(2)} style={style} />
         </div>
     )
 }
@@ -54,25 +111,63 @@ JyankenBox.propTypes = {
     actionPon:PropTypes.func
 }
 
-const ScoreBox = (props) => {
+// const ScoreBox = (props) => {
+//     const teString = ["グー","チョキ","パー"]
+//     const judgmentString = ["引き分け","勝ち","負け"]
+//     return(
+//      <table>
+//          <tbody>
+//              <tr><th>あなた</th><td>{teString[props.human]}</td></tr>
+//              <tr><th>Computer</th><td>{teString[props.computer]}</td></tr>
+//              <tr><th>勝敗</th><td>{judgmentString[props.judgment]}</td></tr>
+//          </tbody>
+//      </table>   
+//     )
+// }
+// ScoreBox.propTypes = {
+//     human: PropTypes.number,
+//     computer: PropTypes.number,
+//     judgment: PropTypes.number,
+// }
+
+
+const ScoreList = (props) => (
+    <Table>
+        <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+            <TableRow>
+                <TableHeaderColumn>時間</TableHeaderColumn><TableHeaderColumn>人間</TableHeaderColumn><TableHeaderColumn>コンピューター</TableHeaderColumn><TableHeaderColumn>結果</TableHeaderColumn>
+            </TableRow>    
+        </TableHeader>
+        <TableBody>
+            {props.scores.map((score,ix) => <ScoreListItem key = {ix} score={score} />)}
+        </TableBody>
+    </Table>
+)
+
+ScoreList.propTypes={
+    scores: PropTypes.array
+}
+
+
+const ScoreListItem=(props) => {
     const teString = ["グー","チョキ","パー"]
     const judgmentString = ["引き分け","勝ち","負け"]
-    return(
-     <table>
-         <tbody>
-             <tr><th>あなた</th><td>{teString[props.human]}</td></tr>
-             <tr><th>Computer</th><td>{teString[props.computer]}</td></tr>
-             <tr><th>勝敗</th><td>{judgmentString[props.judgment]}</td></tr>
-         </tbody>
-     </table>   
+    const dateHHMMSS = (d) => d.toTimeString().substr(0,8)
+    return (
+        <TableRow style={judgmentStyle(props.score.judgment)}>
+            <TableRowColume>{dateHHMMSS(props.score.created_at)}</TableRowColume>
+            <TableRowColume>{teString[props.score.human]}</TableRowColume>
+            <TableRowColume>{teString[props.score.computer]}</TableRowColume>
+            <TableRowColume>{judgmentString[props.score.judgment]}</TableRowColume>
+        </TableRow>
     )
 }
 
-ScoreBox.propTypes = {
-    human: PropTypes.number,
-    computer: PropTypes.number,
-    judgment: PropTypes.number,
+ScoreListItem.propTypes = {
+    score: PropsTypes.object
 }
+
+const judgmentStyle = (judgment) => ({color: ["#000","#2979FF","#FF1744"][judgment]})
 
 ReactDOM.render(
     <JyankenGamePage />,
--- money_book_react/src/index.js
+++ money_book_react/src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import './index.css'
@@ -13,8 +14,7 @@ class MoneyBook extends Component {
 
 
     componentDidMount() {
-        this.setState({
-            books: [
+        this.setState({books: [
                 { date: "1/1", item: "お年玉", amount: 10000 },
                 { date: "1/3", item: "ケーキ", amount: -500 },
                 { date: "2/1", item: "小遣い", amount: 3000 },
@@ -30,7 +30,7 @@ class MoneyBook extends Component {
     render() {
         return (
             <div>
-                <title>小遣い帳</title>
+                <Title>小遣い帳</Title>
                 <MoneyBookList books={this.state.books} />
                 <MoneyEntry add={(date, item, amount) => this.addBook(date, item, amount)} />
             </div>
@@ -42,44 +42,92 @@ class MoneyBook extends Component {
 class MoneyEntry extends Component{
     constructor(props){
         super(props)
-        this.state = {date:'', item:'', amount:'',payingIn:true }
+        this.date = null
+        this.item = null
+        this.amount = null
+        this.payingIn = null
     }
 
-    onChangeDate(event){
-        this.setState({date: event.target.value})
-    }
-    onChangeItem(event){
-        this.setState({item: event.target.value})
-    }
-    onChangeAmount(event){
-        this.setState({amount: event.target.value})
-    }
-    onChanePayingIn(event){
-        this.setState({payingIn: event.target.value=="on"})
-    }
+    //     this.state = {date:'', item:'', amount:'',payingIn:true }
+    // }
+    // onChangeValue(event){
+    //     this.setState({[event.target.name] : event.target.value})
+    // }
+    // onChangeDate(event){
+    //     this.setState({date: event.target.value})
+    // }
+    // onChangeItem(event){
+    //     this.setState({item: event.target.value})
+    // }
+    // onChangeAmount(event){
+    //     this.setState({amount: event.target.value})
+    // }
+    // onChanePayingIn(event){
+    //     this.setState({payingIn: event.target.value=="on"})
+    // }
+
     onClickSubmit(){
-        this.props.add(this.state.date, this.state.item, this.state.amount*(this.state.payingIn ? 1: -1))
-        this.setState({date: '',item: '',amount: '', payingIn:false})
+        // this.props.add(this.state.date, this.state.item, this.state.amount*(this.state.payingIn ? 1 : -1))
+        // this.setState({date: '', item: '', amount: '', payingIn:false})
+        this.props.add(this.date.value, this.item.value, this.amount.value*(this.payingIn.checked ? 1 : -1))
+        this.date.value = ""
+        this.item.value = ""
+        this.amount.value = ""
+        this.payingIn.checked = true
     }
     render(){
         return (
             <div className="entry">
                 <fieldset>
                     <legend>記帳</legend>
+                    {/* <select value={this.state.payingIn ? "on" : "off"} onChange={(e) =>this.onChanePayingIn(e) } >
+                        <option value="on">入金</option>
+                        <option value="off">出金</option>
+                    </select> */}
                     <div>
-                        <input type="radio" value="on" checked={this.state.payingIn} onChange={(event) => this.onChanePayingIn(event) } />入金
-                        <input type="radio" value="off" checked={!this.state.payingIn} onChange={(event) => this.onChanePayingIn(event) } />出金
+                        {/* <input type="radio" value="on" checked={this.state.payingIn} onChange={(event) => this.onChanePayingIn(event) } />入金
+                        <input type="radio" value="off" checked={!this.state.payingIn} onChange={(event) => this.onChanePayingIn(event) } />出金 */}
+                        
+                        <input type="radio" defaultChecked name="payingInOut" ref={(node) => this.payingIn = node} />入金
+                        <input type="radio" name="payingInOut" />出金
                     </div>
-                    <div>日付: <input type="text" value={this.state.date} onChange={(event)=>this.onChangeDate(event)} placeholder="3/15" /> </div>
-                    <div>項目: <input type="text" value={this.state.item} onChange={(event)=>this.onChangeItem(event)} placeholder="おこづかい" /> </div>
-                    <div>金額: <input type="text" value={this.state.amount} onChange={(event)=>this.onChangeAmount(event)} placeholder="1000" /> </div>
-                    <div><input type="submit" value="追加" onClick={()=> this.onClickSubmit()} /> </div>
-                    
+
+                     {/* <div>日付: <input type="text" value={this.state.date} name="date" onChange={(e)=>this.onChangeValue(e)} placeholder="3/15" /> </div>
+                    <div>項目: <input type="text" value={this.state.item} name="item" onChange={(e)=>this.onChangeValue(e)} placeholder="おこづかい" /> </div>
+                    <div>金額: <input type="text" value={this.state.amount} name="amount" onChange={(e)=>this.onChangeValue(e)} placeholder="1000" /> </div>
+                    <div><input type="submit" value="追加" onClick={()=> this.onClickSubmit()} /> </div>  */}
+
+                    <div>日付: <input type="text" defaultValue="" ref={(node) => this.date=node } placeholder="3/15" /> </div>
+                    <div>項目: <input type="text" defaultValue="" ref={(node) => this.item=node } placeholder="おこづかい" /> </div>
+                    {/* <div>項目: <textarea value={this.state.item} name="item" onChange={(e) => this.onChangeValue(e)} placeholder="おこづかい" /></div> */}
+                    <div>金額: <input type="text" defaultValue="" ref={(node) => this.amount=node } placeholder="1000" /> </div>
+                    <div><input type="submit" value="追加" onClick={() => this.onClickSubmit() } /></div>
                 </fieldset>
             </div>
         )
     }
 }
+
+// class Fruits extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state={basket: []}
+//     }
+//     onChangeValue(event){
+//         this.setState({basket: Array.from(event.target.options).filter((e)=>e.selected).map((e)=> e.value)})
+//     }
+//     render(){
+//         return(
+//             <select multiple value={this.state.basket} onChange={(e)=> this.onChangeValue(e)}>
+//                 <option>Orange</option>
+//                 <option>Apple</option>
+//                 <option>Grape</option>
+//             </select>
+//         )
+//     }
+// }
+
+
 
 MoneyEntry.propTypes = {
     add: PropTypes.func.isRequired
@@ -93,12 +141,17 @@ const MoneyBookList = (props) => {
                     <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
                 </thead>
                 <tbody>
-                    {props.book.map((book) => <MoneyBookItem book={book} key={book.date + book.item} /> )}
+                    {props.books.map((book) => <MoneyBookItem book={book} key={book.date + book.item} /> )}
                 </tbody>
             </table>
         </div>
     )
 }
+
+MoneyBookList.propTypes = {
+    books: PropTypes.array.isRequired
+}
+
 
 
 // const MoneyBook = () => {
@@ -127,26 +180,26 @@ const MoneyBookList = (props) => {
 //         </div>
 //     )
 // }
-// const MoneyBookItem = (props) => {
-//     const{date, item, amount}= props.book
-//     return(
-//         <tr>
-//             <td>{date}</td>
-//             <td>{item}</td>
-//             <td>{amount >= 0 ? amount : null}</td>
-//             <td>{amount < 0 ? -amount : null}</td>
-//         </tr>
-//     )
-// }
-// MoneyBookItem.propTypes = {
-//     book: PropTypes.object.isRequired
-// }
 
 
-
-MoneyBookList.propTypes = {
-    book: PropTypes.array.isRequired
+const MoneyBookItem = (props) => {
+    const{date, item, amount}= props.book
+    return(
+        <tr>
+            <td>{date}</td>
+            <td>{item}</td>
+            <td>{amount >= 0 ? amount : null}</td>
+            <td>{amount < 0 ? -amount : null}</td>
+        </tr>
+    )
 }
+MoneyBookItem.propTypes = {
+    book: PropTypes.object.isRequired
+}
+
+
+
+
 
 const Title = (props) => {
     return (<h1>{props.children}</h1>)
